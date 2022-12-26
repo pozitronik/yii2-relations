96a62836aeeee8876f334ad6d3386a96cbe4bab3
Pozitronik <antikillerxxl@gmail.com>
2022-12-26 21:50:47 +0400

    issue #13 Basic features tests

--- tests/Unit/BasicTest.php
+++ tests/Unit/BasicTest.php
@@ -6,10 +6,14 @@ namespace Tests\Unit;
 use app\fixtures\BooksFixture;
 use app\fixtures\UsersFixture;
 use app\models\Books;
+use app\models\RelUsersToBooks;
 use app\models\Users;
 use Codeception\Test\Unit;
+use pozitronik\relations\traits\RelationsTrait;
+use ReflectionClass;
 use Tests\Support\Helper\MigrationHelper;
 use Tests\Support\UnitTester;
+use Yii;
 
 /**
  * ExampleTest
@@ -22,7 +26,7 @@ class BasicTest extends Unit {
 	 * @return string[]
 	 */
 	public function _fixtures() {
-		MigrationHelper::migrate();
+		MigrationHelper::migrateFresh();
 		return [
 			'users' => UsersFixture::class,
 			'books' => BooksFixture::class
@@ -35,6 +39,109 @@ class BasicTest extends Unit {
 	public function testBasic():void {
 		static::assertCount(4, Users::find()->all());
 		static::assertCount(10, Books::find()->all());
+	}
+
+	/**
+	 * @return void
+	 */
+	public function testCreateRelationsViaId():void {
+		/** @var Users $user */
+		$user = Users::find()->where(['login' => 'admin'])->one();
+		$user->relatedBooks = [1, 5, 7];
+
+		$user->save();
+
+		$relations = RelUsersToBooks::find()->all();
+		static::assertCount(3, $relations);
+
+		$user->refresh();
+		static::assertCount(3, $user->relatedBooks);
+		static::assertInstanceOf(Books::class, $user->relatedBooks[0]);
+	}
+
+	/**
+	 * @return void
+	 */
+	public function testCreateRelationsViaModels():void {
+		/** @var Users $user */
+		$user = Users::find()->where(['login' => 'admin'])->one();
+		$user->relatedBooks = [Books::find()->where(['id' => 2])->one(), Books::find()->where(['id' => 6])->one()];
+		$user->save();
+
+		$relations = RelUsersToBooks::find()->all();
+		static::assertCount(2, $relations);
+
+		$user->refresh();
+		static::assertCount(2, $user->relatedBooks);
+		static::assertInstanceOf(Books::class, $user->relatedBooks[0]);
+	}
+
+	/**
+	 * @return void
+	 */
+	public function testCreateRelationsViaMixed():void {
+		/** @var Users $user */
+		$user = Users::find()->where(['login' => 'admin'])->one();
+		$user->relatedBooks = [1, '2', Books::find()->where(['id' => 3])->one()];
+		$user->save();
+
+		$relations = RelUsersToBooks::find()->all();
+		static::assertCount(3, $relations);
+
+		$user->refresh();
+		static::assertCount(3, $user->relatedBooks);
+		static::assertInstanceOf(Books::class, $user->relatedBooks[0]);
+	}
+
+	/**
+	 * @return void
+	 */
+	public function testRelationsSaveAfterPrimary():void {
+		Yii::$app->set('relations', [
+			'class' => RelationsTrait::class,
+			'afterPrimaryMode' => true
+		]);
+
+		$reflectedClass = new ReflectionClass(RelationsTrait::class);
+		$reflectedClass->setStaticPropertyValue('_modeAfterPrimary', null);
+
+
+		/** @var Users $user */
+		$user = Users::find()->where(['login' => 'admin'])->one();
+		$user->relatedBooks = [2, 4, 10];
+		$relations = RelUsersToBooks::find()->all();
+		static::assertCount(0, $relations);
+		static::assertCount(0, $user->relatedBooks);
+
+		$user->save();
+
+		$relations = RelUsersToBooks::find()->all();
+		static::assertCount(3, $relations);
+
+		$user->refresh();
+		static::assertCount(3, $user->relatedBooks);
+		static::assertInstanceOf(Books::class, $user->relatedBooks[0]);
+	}
+
+	/**
+	 * @return void
+	 */
+	public function testEditRelations():void {
+
+	}
+
+	/**
+	 * @return void
+	 */
+	public function testDeleteRelations():void {
+
+	}
+
+	/**
+	 * @return void
+	 */
+	public function testDeleteModel():void {
+
 	}
 
 }
